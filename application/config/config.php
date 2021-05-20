b2866074987f6ae950d3e16f48c969946ce20f45
Nandana <33281538+nandanasugn@users.noreply.github.com>
2021-05-20 23:11:45 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://masjidbersih.test/';
+$config['base_url'] = 'https://masjidbersih.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
